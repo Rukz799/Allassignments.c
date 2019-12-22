8acc0b7d35566bc69a9bac3ef1dd6c7e31b28a63
Rukhsana Mengal <56469719+Rukz799@users.noreply.github.com>
2019-12-22 10:44:15 +0500

    Create ConvrrtrPseudocode.c

---
+++ ConvrrtrPseudocode.c
@@ -0,0 +1,22 @@
+*********** PSEUDO CODE TO CONVERT Meter to inch**************
+
+1- Display a message "Enter value in meter".
+
+2. Take input from the user.
+
+3. Display available choices.
+
+4. Take input from user. (make choice)
+
+5. Perform conversion.
+
+6. If (choice is 1) then,
+7. result=
+
+8. If (choice is 2) then,
+9.  result=
+
+10. If (choice is 0) then,
+11.  exit
+
+12. Display result.
